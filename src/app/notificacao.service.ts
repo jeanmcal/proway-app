f0ceedd98b19e55bc6ace276833862d26a364d97
Jean <jean-design@hotmail.com>
2023-06-02 17:33:09 -0300

    build

---
+++ src/app/notificacao.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacaoService {
+
+  constructor(
+    private snackBar: MatSnackBar 
+    ) { }
+
+
+notificar (message: string) {
+  this.snackBar.open(message,"ok,", {
+    duration: 200,
+    verticalPosition: "top",
+    horizontalPosition: "center"
+  });
+}
+
+}
